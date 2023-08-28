c9e019b0253ec16501fd2bdebd117d1a2004eaed
RuthalaKiran <125887544+RuthalaKiran@users.noreply.github.com>
2023-08-28 20:20:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const  createbtn = document.querySelector(".createbtn");
+const  paracontainer = document.querySelector(".para-container");
+let notes = document.querySelectorAll(".notes");
+
+createbtn.addEventListener("click", ()=>{
+    let p = document.createElement("p");
+    let img = document.createElement("img");
+    p.className = "notes";
+    p.setAttribute("contenteditable","true");
+    img.src = "images/delete.png";
+    paracontainer.appendChild(p).appendChild(img);
+    update();
+})
+paracontainer.addEventListener("click", function(e){
+    if(e.target.tagName == "IMG"){
+        e.target.parentElement.remove();
+        update();
+    }
+    else if(e.target.tagName == "P"){
+     notes = document.querySelectorAll(".notes");
+        notes.forEach(n =>{
+            n.onkeyup = function(){
+                update();
+            }
+        })
+    }
+
+})
+
+function update(){
+    localStorage.setItem("data",paracontainer.innerHTML);
+}
+function show(){
+    paracontainer.innerHTML = localStorage.getItem("data");
+}
+show();
